681e7b94abce82841552ba703e64a4e563ff8b32
rock-007 <59394025+rock-007@users.noreply.github.com>
2021-01-05 00:40:55 +0000

    Class files added

---
+++ src/BankAccountApp/Accounts.java
@@ -0,0 +1,9 @@
+package BankAccountApp;
+
+public abstract class Accounts implements IBaseRate {
+    //List common properties for saving and checking account
+
+    //Constructor to set base properties and initialise the account
+
+    // List Common methods
+}
--- src/BankAccountApp/Savings.java
+++ src/BankAccountApp/Savings.java
@@ -1,4 +1,10 @@
 package BankAccountApp;
 
 public class Savings {
+    // List Properties specific to the Saving account
+
+    //Constructor to initialise settings for the savings properties
+
+    // List any methods specific to saving account
+
 }
